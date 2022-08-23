78f9a1a671257a4f6596a5ce9449136f7d55e4e4
Abhishri123 <vithalaniabhishree@gmail.com>
2022-08-23 11:14:09 +0530

    Change

--- resources/views/bookings/edit.blade.php
+++ resources/views/bookings/edit.blade.php
@@ -28,72 +28,7 @@
         <form action="{{ route('bookings.update',$booking->id) }}" method="POST" enctype="multipart/form-data">
             @csrf
             @method('PUT')
-            <!-- <div class="row">
-
-
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <strong>Full Name:</strong>
-                        <input type="text" name="full_name" class="form-control" placeholder="Enter Full Name" value="{{ old('full_name') }}">
-                        @if ($errors->has('full_name'))
-                            <span class="text-danger">{{ $errors->first('full_name') }}</span>
-                        @endif
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <strong>Pax:</strong>
-                        <input type="pax" name="pax" class="form-control" placeholder="pax" value="{{ old('pax') }}">
-                        @if ($errors->has('pax'))
-                            <span class="text-danger">{{ $errors->first('pax') }}</span>
-                        @endif
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <strong>Contact:</strong>
-                        <input type="text" name="contact" class="form-control" placeholder="Contact" value="{{ old('contact') }}">
-                        @if ($errors->has('contact'))
-                            <span class="text-danger">{{ $errors->first('contact') }}</span>
-                        @endif
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <strong>Transport vehicle:</strong>
-                        <input type="text" name="transport_vehicle" class="form-control" placeholder="Transport Vehicle" value="{{ old('transport_vehicle') }}">
-                        @if ($errors->has('transport_vehicle'))
-                            <span class="text-danger">{{ $errors->first('transport_vehicle') }}</span>
-                        @endif
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-
-                <div class="col-md-6">
-                <div class="form-group">
-                        <strong>Transport vehicle:</strong>
-                        <input type="text" name="booking_for" class="form-control" placeholder="booking for" value="{{ old('booking_for') }}">
-                        @if ($errors->has('booking_for'))
-                            <span class="text-danger">{{ $errors->first('booking_for') }}</span>
-                        @endif
-                    </div> 
-                </div>
-                <div class="col-md-6">
-                <div class="form-group">
-                        <strong>Duration:</strong>
-                        <input type="text" name="duration" class="form-control" placeholder="duration" value="{{ old('duration') }}">
-                        @if ($errors->has('duration'))
-                            <span class="text-danger">{{ $errors->first('duration') }}</span>
-                        @endif
-                    </div> 
-                </div>
-            </div>
-                <button type="submit" class="btn btn-primary ml-3">Submit</button>
-            </div> -->
-
+            
             <div class="row">
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
